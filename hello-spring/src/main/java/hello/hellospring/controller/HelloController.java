1a729ac2b9e3a6a057838044185f7d912e6b2c3e
Suyeonkim07 <ksy4836@gmail.com>
2024-05-21 22:38:31 +0900

    순수 jdbc 연결코드

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class helloController {
+public class HelloController {
 
 	@GetMapping("hello")
 	public String hello(Model model) {
